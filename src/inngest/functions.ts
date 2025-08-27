1d46a127ce5a2129670506211ffeae42f848007f
Stuart Smith <stuarts205@gmail.com>
2025-08-27 15:28:54 -0600

    05: ai jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,21 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const agent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next js developer.  You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4o" }),
+    });
+
+    const { output } = await agent.run(
+      `Write the following snippets: ${event.data?.value} `
+    );
+    console.log(output);
+
+    return { output };
+  }
 );
