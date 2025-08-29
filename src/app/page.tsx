570b24c904c9be900eaa0224986e6b014b15184c
Stuart Smith <stuarts205@gmail.com>
2025-08-29 16:20:56 -0600

    08: messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,34 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "sonner";
 
 const Page = () => {
   const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  const {data: message} = useQuery(trpc.messages.getMany.queryOptions())
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        toast.success("Message created");
+      },
+    })
+  );
 
   return (
-    <div className='p-4 max-w-7xl mx-auto'>
+    <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button onClick={() => invoke.mutate({ value: value })}>
+      <Button
+        disabled={createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
+      >
         Invoke Background Job
       </Button>
+      {JSON.stringify(message, null, 2)}
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,32 +1,7 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
-import { inngest } from '@/inngest/client';
+import { createTRPCRouter } from '../init';
+import { messagesRouter } from '@/modules/messages/server/procedures';
 export const appRouter = createTRPCRouter({
-  invoke: baseProcedure
-    .input(
-      z.object({
-        value: z.string(),
-      })
-    )
-    .mutation(async ({input}) => {
-      await inngest.send({
-        name: 'test/hello.world',
-        data: {
-          value: input.value,
-        },
-      })
-    }),
-  create: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `Hello ${opts.input.text}`,
-      };
-    }),
+  messages: messagesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
